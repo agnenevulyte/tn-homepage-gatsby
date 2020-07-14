4c8a0d286197d7d8fad657fedc1a760cfec6392b
Agne Nevulyte <agnenevulyte@Agnes-Air.connect>
2020-07-14 11:08:21 +0100

    first commit

---
+++ src/components/images/svgs/burger-nav.js
@@ -0,0 +1,13 @@
+
+
+function BurgerNavSVG() {
+    return (
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+                <path fill="white" stroke="#fff" stroke-width=".3" d="M.9695 11.1805h22.061a.8195.8195 0 1 1 0 1.639H.9695a.8195.8195 0 0 1 0-1.639zM.9695 3.8268h22.061a.8195.8195 0 1 1 0 1.639H.9695a.8195.8195 0 0 1 0-1.639zM.9695 18.5342h22.061a.8195.8195 0 1 1 0 1.639H.9695a.8195.8195 0 0 1 0-1.639z" />
+            </svg>
+
+        </div>
+    );
+}
+export default BurgerNavSVG
