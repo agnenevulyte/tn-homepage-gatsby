bdc486723acd760ae726ccaa744d9a043e16170a
Agne Nevulyte <agnenevulyte@Agnes-Air.connect>
2020-07-15 21:48:52 +0100

    fetching data from prismic

---
+++ gatsby-node.js
@@ -0,0 +1,26 @@
+const fetch = require(`node-fetch`)
+
+exports.sourceNodes = async ({
+  actions: { createNode },
+  createContentDigest,
+}) => {
+  // get data from GitHub API at build time
+  const result = await fetch(`https://ra9qbnj3ah.execute-api.eu-west-2.amazonaws.com/fin/marketing/docs/home-page`)
+  const resultData = await result.json()
+  console.log('resultData', resultData)
+  // create node for build time data example in the docs
+  createNode({
+    // nameWithOwner and url are arbitrary fields from the data
+    uid: resultData.uid,
+    type: resultData.type,
+    data: resultData.data,
+    // required fields
+    id: `example-build-time-data`,
+    parent: null,
+    children: [],
+    internal: {
+      type: `TN`,
+      contentDigest: createContentDigest(resultData),
+    },
+  })
+}
--- src/pages/account.js
+++ src/pages/account.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { Link, graphql } from "gatsby"
+import { Link, graphql } from "gatsby";
+import { PrismicTypes } from '../components/prismictypes';
 
-export default function Tetst({data}) {
+export default function Tetst({ data }) {
   return (
-    <div style={{ color: `teal` }}>
-        <Link to='/'>Home</Link>
-        <p>API TEST</p>
-        <h1>{data.allMultiApiSourceRestRockets.edges[0].node.country}</h1>
-        <h1>{data.allMultiApiSourceUsers.edges[0].node.name}</h1>
-        <h1>{data.allMultiApiSourceFin}</h1>
+    <div className="page page-homepage" style={{ color: `teal` }}>
+      <Link to='/'>Home</Link>
+      <p>API TEST</p>
+      <h1>{data.allMultiApiSourceRestRockets.edges[0].node.country}</h1>
+      <h1>{data.allMultiApiSourceUsers.edges[0].node.name}</h1>
+      <h1>{data.tn.uid}</h1>
+      <h1>{data.tn.type}</h1>
+      <h1>{data.tn.data.title}</h1>
+
+
+
     </div>
+    
   )
 }
 
@@ -23,12 +30,79 @@ query MyQuery {
       }
     },
     allMultiApiSourceUsers {
-        edges {
-          node {
-            name
-          }
+      edges {
+        node {
+          name
         }
-      },
+      }
+    },
+    tn {
+        type
+        uid
+        data {
+          footer {
+            data {
+              copyright
+              contact {
+                tel_number
+              }
+              faqs {
+                display_value
+                url
+              }
+              footer_logo {
+                alt
+                dimensions {
+                  height
+                  width
+                }
+                url
+              }
+              footer_social {
+                icon_name
+                url
+              }
+              legal {
+                body {
+                  spans {
+                    data {
+                      id
+                      isBroken
+                      lang
+                      link_type
+                      slug
+                      tags
+                      type
+                      uid
+                    }
+                    end
+                    start
+                    type
+                  }
+                  text
+                  type
+                }
+              }
+              meta_links {
+                display_value
+                url
+              }
+              name
+            }
+            id
+            isBroken
+            lang
+            link_type
+            slug
+            type
+          }
+          meta_description
+          name
+          slug
+          title
+          visibility
+        }
+    },
   }
   
 `
