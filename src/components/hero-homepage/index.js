439b652fa2aab9a39662cf914666f0bc13fc4535
Agne Nevulyte <agnenevulyte@Agnes-Air.connect>
2020-07-16 14:41:10 +0100

    HeroHomePage still doesn't like richtext

--- src/components/hero-homepage/index.js
+++ src/components/hero-homepage/index.js
@@ -1,23 +1,23 @@
 import { RichText } from "prismic-reactjs";
 import React from 'react';
-// import { DynamicIcon } from '../index';
-// import Link from 'gatsby';
-// import styled from "styled-components";
+import { DynamicIcon } from '../index';
+import Link from 'gatsby';
+import styled from "styled-components";
 
 
 function HeroHomepage({ data }) {
 
-    // const getImageTemplate = image => {
-    //     const ImageComponent = styled.div`
-    //       background-image: url(${image.mobile.url});
-    //       @media (min-width: 480px) {
-    //         background-image: url(${image.url});
-    //       }}
-    //       `;
-    //     return (
-    //         <ImageComponent className="hero-homepage__image" title={image.alt} />
-    //     );
-    // }
+    const getImageTemplate = image => {
+        const ImageComponent = styled.div`
+          background-image: url(${image.mobile.url});
+          @media (min-width: 480px) {
+            background-image: url(${image.url});
+          }}
+          `;
+        return (
+            <ImageComponent className="hero-homepage__image" title={image.alt} />
+        );
+    }
 
     console.log('yay hero home page is rendering!!!!');
     console.log('data.items---', data.items);
